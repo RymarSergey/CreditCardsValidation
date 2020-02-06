41062c74a30137d9181cc92903d8e2e94ab9d529
Sergey <sergey.rymar.ne@yandex.ru>
2020-02-06 22:36:34 +0200

    first comit credit Card

--- creditCards.go
+++ creditCards.go
@@ -18,14 +18,12 @@ func main() {
 
 	j_sonString := `{
 		"pan" : "5375414112236376",
-		"valid_thru" : "01/23",
+		"valid_thru" : "31/23",
 		"owner" : "Rymar Sergey",
 		"cvc_2" : "123"
 	}`
 	cardParse := creditCard{}
 	owner := json.Unmarshal([]byte(j_sonString), &cardParse)
-	//s := "444-111444382 5037" //mono
-	//s:="5375414112236376"//mono
 	if owner != nil {
 	}
 	fmt.Println(IsCreditCardValid(cardParse.Pan))
@@ -37,7 +35,6 @@ func IsCreditCardValid(s string) bool {
 	s = strings.ReplaceAll(s, "-", "")
 	var sum int64 = 0
 	for i, j := len(s)-1, 1; i >= 0; i-- {
-
 		temp, r := strconv.ParseInt(string(s[i]), 10, 8)
 		if r != nil {
 			sum = 0

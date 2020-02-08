f48858821edda735ec9a35c357d27cdbe849da68
Sergey <sergey.rymar.ne@yandex.ru>
2020-02-08 08:20:11 +0200

    add j incrementation into "for" defenition

--- creditCards.go
+++ creditCards.go
@@ -17,7 +17,7 @@ type creditCard struct {
 func main() {
 
 	jSonString := `{
-		"pan" : "5375414112236376",
+		"pan" : "5375414874236376",
 		"valid_thru" : "31/23",
 		"owner" : "Rymar Sergey",
 		"cvc_2" : "123"
@@ -39,20 +39,32 @@ func IsCreditCardValid(cardPan string) bool {
 	}
 	var sum int64 = 0
 
-	for i, j := len(cardPan)-1, 1; i >= 0; i-- {
-		temp, r := strconv.ParseInt(string(cardPan[i]), 10, 8)
+	for i, value := range cardPan {
+		temp, r := strconv.ParseInt(string(value), 10, 8)
 		if r != nil {
 			sum = 0
 			break
 		}
-		if j%2 == 0 {
-			temp = temp * 2
-			if temp > 9 {
-				temp -= 9
+		if len(cardPan)%2 == 0 {
+			if i%2 == 0 {
+				temp = chengeDigit(temp)
+			}
+		} else {
+			if i%2 == 1 {
+				temp = chengeDigit(temp)
 			}
 		}
-		j++
 		sum += temp
 	}
 	return (sum != 0) && (sum%10 == 0)
+}
+
+func chengeDigit(temp int64) int64 {
+
+	temp = temp * 2
+	if temp > 9 {
+		temp -= 9
+
+	}
+	return temp
 }
